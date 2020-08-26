9056bbaa6cc93c3e9eb42129b108eb9cfbcc7511
Vincent Servio <vincentservio@gmail.com>
2020-08-26 00:42:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Your code goes here
-document.addEventListener("DOMContentLoaded", function (){)
-  
-}
+document.addEventListener("DOMContentLoaded", function (){
+  document.querySelector("")
+)}
